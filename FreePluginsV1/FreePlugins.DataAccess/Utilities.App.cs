d41d49219d0c053d0403b434cd9e9b358ed6b9f1
Daniel Pepka <pepkad@wsu.edu>
2026-01-09 09:56:32 -0800

    Added basic systems.  Initial clone of the FreeCRM and base version. Initial clone of the FreeTools and guide

---
+++ FreePluginsV1/FreePlugins.DataAccess/Utilities.App.cs
@@ -0,0 +1,6 @@
+namespace FreePlugins;
+
+public static partial class Utilities
+{
+    // Add your app-specific utility methods here.
+}
---
+++ FreePluginsV1/FreePlugins.DataObjects/GlobalSettings.App.cs
@@ -0,0 +1,6 @@
+namespace FreePlugins;
+
+public static partial class GlobalSettings
+{
+    // Add any app-specific global settings here.
+}
